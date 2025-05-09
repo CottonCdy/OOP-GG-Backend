3c98a1e401aa6ff05f5342a8b33fb4425be901ea
JS <taste397@naver.com>
2025-05-09 23:06:09 +0900

    First commit

---
+++ src/main/java/com/my/oopgg/OopggApplication.java
@@ -0,0 +1,13 @@
+package com.my.oopgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/oopgg/OopggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.oopgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
